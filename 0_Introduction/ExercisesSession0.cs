621b0bef9d8fcc553df4f6bd86aa53dbb0ef896e
JulianBesems <36232964+JulianBesems@users.noreply.github.com>
2021-10-06 01:26:05 +0100

    Add files via upload

---
+++ 0_Introduction/ExercisesSession0.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+class MainClass {
+    public static void Main(string[] args) {
+
+        // change the Exercise00 to the exercise name you want to test (corresponds to the class name below)
+
+        Exercise08.run();
+    }
+}
+
+class Exercise00 {
+
+    // For this exercise:
+    // 1. Run this script:
+    //      - Right click on the project in the menu on the left
+    //        (If the menu with filenames is not visible, select View -> Design)
+    //      - Select Run Project.
+    //      - Or run this file with Ctrl + f5 (especially if the output window closes immediately)
+    // 2. If you did this correctly you should see the output at the bottom saying: Hello world
+    // 3. Add another line of output with your name and run the script again.
+
+    public static void run() {
+        // Enter your code here
+
+    }
+}
+
+class Exercise01 {
+
+    // 1. Write a bit of code that prints “Introduction to Programming” to the console.
+    // 2. Run the script.
+
+    public static void run() {
+        // Enter your code here
+    }
+}
+
+class Exercise02 {
+
+    // 2. Create a piece of code to print your name, email, age and gender on seperate lines.
+    //    For each element first assign it to an oppropriate variable, and use the variable to print.
+
+    // (Bonus if you can do the print statement for all variables in one line of code.
+    // Hint: "\n" is the character for a new line)
+
+    public static void run() {
+        // Enter your code here
+
+    }
+}
+
+class Exercise03 {
+
+    // 3. Print the numbers 0, 178, -21 and 12^20 on the console.
+    // Hint: Google for how to calculate exponential powers in C#. 
+
+    public static void run() {
+        // Enter your code here
+        
+    }
+}
+
+class Exercise04 {
+
+    // 4. Print sin(200), cos(100), tan(pi/4)
+    // Hint: Google for how to use trigonometric function, and how to get the value of pi.  
+
+    public static void run() {
+        // Enter your code here
+        
+    }
+}
+
+class Exercise05 {
+
+    // 5. Write a program to read your first and last names from the console seperately,
+    //    and then print them on the console together, separated by a space.
+
+    // Note: On Mac, in order to be able to enter text on the console, try to run it with Ctrl + f5, otherwise:
+    //          - Right click project
+    //          - Select Run With -> Custom Configuration...
+    //          - Check "Run In Terminal Pad" and press Run.
+    //          - Now you should be able to type in the console  
+
+    public static void run() {
+        // Enter your code here
+        
+    }
+}
+
+class Exercise06 {
+
+    // 5. Write a program that determins whether a number given as user input is
+    // positive or negative
+    // You will need to convert the console input from a string to a number first!
+
+    public static void run() {
+        // Enter your code here
+        
+    }
+}
+
+class Exercise07 {
+    // Write a program that picks a random number between 1-20 and makes the
+    // use guess until they get the number right. Then print a congratulations
+    // message
+    // (Find out yourself how to generate a random integer)
+    // Bonus: make the user choose the range within which they have to guess
+    // Bonus: keep track of how many guesses were made and print this at the end
+
+    public static void run() {
+        // Enter your code here
+        
+    }
+}
+
+class Exercise08 {
+    // Write a program using for loops to print a christmas tree of x lines high
+    // specified by the user.
+    // (use for loops)
+    // so for instance, a chrismas tree of 4 high should look like this:
+    //    *
+    //   ***
+    //  *****
+    // *******
+    //    |
+    //
+    // hint: first combine strings into a variable before printing
+
+
+    public static void run() {
+        // Enter your code here
+
+    }
+}
