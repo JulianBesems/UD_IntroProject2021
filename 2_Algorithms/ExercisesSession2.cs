e3e1ac421325490b8c0cd31bbce76fb8582de42a
JulianBesems <36232964+JulianBesems@users.noreply.github.com>
2021-10-08 04:30:44 +0200

    Add files via upload

---
+++ 2_Algorithms/ExercisesSession2.cs
@@ -0,0 +1,38 @@
+using System;
+
+class MainClass {
+    public static void Main(string[] args) {
+
+        Exercise20.run();
+    }
+}
+
+class Exercise20 {
+    // For this exercise:
+    // Define a function that takes an integer as input, prints
+    // all integers that it is divisable by, and returns the largest one.
+    // so for instance, given the number 12, the print output or the function would be:
+
+    // 1
+    // 2
+    // 3
+    // 4
+    // 6
+
+
+    // define the function here after "public static"
+
+    public static
+
+    public static void run() {
+        // Call your function here with an argument n
+        // - save the output to a variable.
+        // - Then print the value of the varible with a phrase saying that
+        // this is the largest number n is divisable by.
+        // so for the example of n = 12 the output here would be:
+
+        // 6 is the largest number that 12 is divisable by.
+        
+
+    }
+}
