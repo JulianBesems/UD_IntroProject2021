fd154786773d755a3a25601df648454c48bf4ec0
JulianBesems <36232964+JulianBesems@users.noreply.github.com>
2021-10-11 14:01:03 +0200

    Add files via upload

---
+++ 3_Arrays/SmallArrayExercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// 1. Write a program that allocates array of 20 integers and initializes each
+// element by its index multiplied by 5. Print the obtained array on the console.
+
+//2. Write a program that creates a 2-Dimensional array of 3 rows and 5 columns
+// where each element of the array is the row index to the power of the column index
+
+class ArrayExercise
+{
+    static void Main()
+    {
+        //Enter code here
+    }
+}
